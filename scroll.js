c207655b748bf5eebe0d2329a6a4f541e04a5b38
ela_lisondra <emrlisondra@gmail.com>
2021-07-10 00:19:09 +0100

    added background colour transition in scroll js

--- scroll.js
+++ scroll.js
@@ -10,4 +10,16 @@ $(document).on('scroll', function() {
         $('header').removeClass('hidden')
     }
 
+
+    if (pixelsFromTop < 600) {
+        $('body').css('background-color', '#fff')
+    } else if (pixelsFromTop < 1400) {
+        $('body').css('background-color', '#a29c97')
+    } else if (pixelsFromTop < 2200) {
+        $('body').css('background-color', '#d9dfe4')
+    } else if (pixelsFromTop < 3000) {
+        $('body').css('background-color', '#fff0f0')
+    } else {
+        $('body').css('background-color', '#cdccc7')
+    }
 })
